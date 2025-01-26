0f141f12d43cadfc34a327103b969c0cd74cff9b
prathapmohan27 <prathapmohan2704@gmail.com>
2025-01-26 16:59:53 +0530

    removed FE and added the readme file

--- slack-client/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-import { SlackRedirectComponent } from './components/slack-redirect/slack-redirect.component';
-
-export const appRoutes: Route[] = [
-  {
-    path: 'slackCallback',
-    component: SlackRedirectComponent,
-  },
-];
